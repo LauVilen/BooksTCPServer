d4ae7bdf77f0c64e2794d5d9d6d9449da3d18a24
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-10-10 21:07:33 +0200

    Add project files.

---
+++ TCPServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BooksTCPServer
+{
+    class TCPServer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server start");
+
+        }
+    }
+}
